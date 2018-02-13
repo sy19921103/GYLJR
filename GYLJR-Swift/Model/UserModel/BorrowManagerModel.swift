4c944673a4f3547786d4185d49931497c8d43f87
iOS <2415879990@qq.com>
2018-02-13 15:09:40 +0800

    2.13

---
+++ GYLJR-Swift/Model/UserModel/BorrowManagerModel.swift
@@ -0,0 +1,13 @@
+//
+//  BorrowManagerModel.swift
+//  GYLJR-Swift
+//
+//  Created by iOS on 2018/2/13.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+import UIKit
+
+class BorrowManagerModel: BaseModel {
+
+}
---
+++ GYLJR-Swift/UI/CustomUI/BaseScrollView.swift
@@ -0,0 +1,21 @@
+//
+//  BaseScrollView.swift
+//  GYLJR-Swift
+//
+//  Created by iOS on 2018/2/13.
+//  Copyright © 2018年 iOS. All rights reserved.
+//
+
+import UIKit
+
+class BaseScrollView: UIScrollView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
